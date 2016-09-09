b5298c671b2750a99114f49e32413b23c01c547a
Amy Wu <acagaga@gmail.com>
2016-09-09 15:42:54 -0400

    stack_practice

---
+++ mini-projects/techinical_interview/stack_practice.py
@@ -0,0 +1,64 @@
+class Element(object):
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+class LinkedList(object):
+    def __ini__(self,head=None):
+        self.head=head
+
+    def append(self,new_element):
+        current=self.head
+        if self.head:
+            while current.next:
+                current=current.next
+            current.next=new_element
+        else:
+            self.head=new_element
+
+    def insert_first(self,new_element):
+        #"Insert new element as the head of the LinkedList"
+        new_element.next=self.head
+        self.head=new_element
+
+    def delete_first(self):
+        #"Delete the first(head) element in the LinkedList"
+        if self.head:
+            deleted_element=self.head
+            temp=deleted_element.next
+            self.head=temp
+            return deleted_element
+        else:
+            return None
+
+class Stack(object):
+    def __init__(self,top=None):
+        self.ll=LinkedList(top)
+    
+    def push(self,new_element):
+        #"Push (add) a new element onto the top of the stack"
+        self.ll.insert_first(new_element)
+
+    def pop(self):
+        # "Pop (remove) the first element off the top of the stack"
+        self.ll.delet_first()
+
+# Test cases
+# Set up some Elements
+e1=Element(1)
+e2=Element(2)
+e3=Element(3)
+e4=Element(4)
+
+# Start setting up a Stack
+stack=Stack(e1)
+
+# Test stack functionality
+stack.push(e2)
+stack.push(e3)
+print stack.pop().value  # should be 3
+print stack.pop().value  # should be 2
+print stack.pop().value  # should be 1
+print stack.pop()        # should be None
+stack.push(e4)
+print stack.pop().value  # should be 4
